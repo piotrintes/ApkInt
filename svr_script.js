be818ec30b92d30435877fbfc60b59917606018d
piotrintes <piotr.intes@gmail.com>
2019-03-11 17:05:02 +0100

    dodanie funkcjonaności

--- svr_script.js
+++ svr_script.js
@@ -1,12 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.statusCode = 200;
+    if (req.url == "/style.css") {
+        fs.readFile('./style.css',(err,d)=>{
+            res.write(d);
+            res.end();
+        });
+    } else if (req.url == "/") {
+        res.setHeader('Content-Type', 'text/plain');
+        console.log(req.url);
+        res.end('Hello World\n');
+    } else {
+        var d;
+        var str = "./index.htm";
+        fs.readFile(str,(err,d)=>{
+            res.write(d);
+            res.end('Zapytano o '+req.url+"\n\n");
+        });
+    }
+    
 });
 
 server.listen(port, hostname, () => {
